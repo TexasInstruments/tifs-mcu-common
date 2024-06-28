d2ac5a504b59d017c396b6d9279ec3a2926d413d
Utkarsh M Kripashankar <u-kripashankar@ti.com>
2024-06-28 15:23:48 +0530

    am263x/am273x/am263px: tools: update tooling for sbl signing
    - add support in sbl tooling for unlocking customer root of trust
      unlock in secure manager.
    - add extension in mcu_rom_image_gen.py named enc_keylock.
    - keylock field in extension if 0x5A will lock active customer MEK
      in secure manager. 0xA5 will unlock SMEK/BMEK.
    
    Fixes: TIFSMCU-2280
    
    Signed-off-by: Utkarsh M Kripashankar <u-kripashankar@ti.com>

--- tools/boot/signing/mcu_rom_image_gen.py
+++ tools/boot/signing/mcu_rom_image_gen.py
@@ -19,23 +19,28 @@ g_sbl_hsm_max_size = 983000
 g_sha_to_use = "sha512"
 
 g_sha_oids = {
-    "sha256" : "2.16.840.1.101.3.4.2.1",
-    "sha384" : "2.16.840.1.101.3.4.2.2",
-    "sha512" : "2.16.840.1.101.3.4.2.3",
-    "sha224" : "2.16.840.1.101.3.4.2.4",
+    "sha256": "2.16.840.1.101.3.4.2.1",
+    "sha384": "2.16.840.1.101.3.4.2.2",
+    "sha512": "2.16.840.1.101.3.4.2.3",
+    "sha224": "2.16.840.1.101.3.4.2.4",
 }
 
 
 g_dbg_types = {
-    "DBG_PERM_DISABLE"   : '0',
-    "DBG_SOC_DEFAULT"    : '1',
-    "DBG_PUBLIC_ENABLE"  : '2',
-    "DBG_FULL_ENABLE"    : '4',
+    "DBG_PERM_DISABLE": '0',
+    "DBG_SOC_DEFAULT": '1',
+    "DBG_PUBLIC_ENABLE": '2',
+    "DBG_FULL_ENABLE": '4',
 }
 
 g_core_types = {
-    "R5"          : '0',
-    "HSM"         : '1',
+    "R5": '0',
+    "HSM": '1',
+}
+
+g_enc_unlock_types = {
+    "LOCK": '90',
+    "UNLOCK": '165',
 }
 
 g_openssl111_x509_template = '''
@@ -63,6 +68,7 @@ basicConstraints = CA:true
 {EXT_ENC_SEQ}
 {DBG_EXT}
 {KD_EXT}
+{ENC_KEYLOCK}
 
 [ boot_seq ]
 certType     =  INTEGER:{CERT_TYPE}
@@ -126,6 +132,7 @@ subjectKeyIdentifier = none
 {EXT_ENC_SEQ}
 {DBG_EXT}
 {KD_EXT}
+{ENC_KEYLOCK}
 
 [ boot_seq ]
 certType     =  INTEGER:{CERT_TYPE}
@@ -140,6 +147,10 @@ shaValue = FORMAT:HEX,OCT:{SHA_VAL}
 
 [ swrv ]
 swrv = INTEGER:{SWRV}
+'''
+g_ext_enc_keylock = '''
+[ enc_keylock ]
+keylock         =  INTEGER:{CUST_MEK_KEY_LOCK}
 '''
 
 g_ext_enc_seq = '''
@@ -163,12 +174,17 @@ g_kd_seq = '''
 kd_salt = FORMAT:HEX,OCT:{KDSALT_VAL}
 '''
 
+
 def get_sha_val(f_name, sha_type):
-    sha_val = subprocess.check_output('openssl dgst -{} -hex {}'.format(sha_type, f_name), shell=True).decode()
+    sha_val = subprocess.check_output(
+        'openssl dgst -{} -hex {}'.format(sha_type, f_name), shell=True).decode()
     return sub("^.*= ", r'', sha_val).strip('\n')
+
 
 def get_cert(args):
     swrev = args.swrv
+    ext_enc_keylock = ""
+
     if(swrev is None):
         swrev = 1
 
@@ -178,11 +194,15 @@ def get_cert(args):
             bootCore_id = 16
             certType = 1
             bootCoreOptions = 0
+            enc_keylock_ext = "1.3.6.1.4.1.294.1.11=ASN1:SEQUENCE:enc_keylock"
+            enc_key_lock = g_enc_unlock_types['UNLOCK']
         else:
             bootAddress = 0
             bootCore_id = 0
             certType = 2
             bootCoreOptions = 0
+            enc_keylock_ext = ""
+            enc_key_lock = g_enc_unlock_types['LOCK']
 
     dbg_seq = ''
     ext_enc_seq = ''
@@ -214,76 +234,88 @@ def get_cert(args):
 
     if args.sbl_enc:
         # SBL encryption is enabled
-        encsbl_name, enc_iv, enc_rs = get_encrypted_file_iv_rs(args.image_bin, args.enc_key)
+        encsbl_name, enc_iv, enc_rs = get_encrypted_file_iv_rs(
+            args.image_bin, args.enc_key)
         ext_enc_seq = "1.3.6.1.4.1.294.1.4=ASN1:SEQUENCE:encryption"
         sbl_enc_seq = g_ext_enc_seq.format(
-                        NUM_COMP = 1,
-                        ENC_IV = enc_iv,
-                        ENC_RS = enc_rs,
-                        ENC_ITER_CNT = enc_iter_count,
-                        ENC_SALT = enc_salt,
-                        extra_enc_comp='',
-                        extra_enc_comp_seq='',
-                        )
+            NUM_COMP=1,
+            ENC_IV=enc_iv,
+            ENC_RS=enc_rs,
+            ENC_ITER_CNT=enc_iter_count,
+            ENC_SALT=enc_salt,
+            extra_enc_comp='',
+            extra_enc_comp_seq='',
+        )
         image_bin_name = encsbl_name
 
     if args.tifs_enc:
-        enctifs_name, enc_iv, enc_rs = get_encrypted_file_iv_rs(args.image_bin, args.enc_key)
+        enctifs_name, enc_iv, enc_rs = get_encrypted_file_iv_rs(
+            args.image_bin, args.enc_key)
         ext_enc_seq = "1.3.6.1.4.1.294.1.4=ASN1:SEQUENCE:encryption"
         tifs_enc_seq = g_ext_enc_seq.format(
-                        NUM_COMP = 1,
-                        ENC_IV = enc_iv,
-                        ENC_RS = enc_rs,
-                        ENC_ITER_CNT = enc_iter_count,
-                        ENC_SALT = enc_salt,
-                        extra_enc_comp='',
-                        extra_enc_comp_seq='',
-                        )
+            NUM_COMP=1,
+            ENC_IV=enc_iv,
+            ENC_RS=enc_rs,
+            ENC_ITER_CNT=enc_iter_count,
+            ENC_SALT=enc_salt,
+            extra_enc_comp='',
+            extra_enc_comp_seq='',
+        )
         image_bin_name = enctifs_name
 
     if args.kd_salt and args.sbl_enc:
         ext_kd_seq = "1.3.6.1.4.1.294.1.5=ASN1:SEQUENCE:key_derivation"
         kd_salt = get_key_derivation_salt(args.kd_salt)
         kd_seq = g_kd_seq.format(
-                    KDSALT_VAL = kd_salt
-                    )
+            KDSALT_VAL=kd_salt
+        )
     ret_cert = ""
 
-    openssl_version: str = str(subprocess.check_output(f"openssl version", shell=True))
+    if(args.core == 'R5'):
+        ext_enc_keylock = g_ext_enc_keylock.format(
+            CUST_MEK_KEY_LOCK=enc_key_lock,
+        )
+
+    openssl_version: str = str(
+        subprocess.check_output(f"openssl version", shell=True))
 
     if "1.1.1" in openssl_version:
-        print(f"WARNING: OpenSSL version {openssl_version.split()[1]} found is not recommended due to EOL. Please install version 3.x .")
+        print(
+            f"WARNING: OpenSSL version {openssl_version.split()[1]} found is not recommended due to EOL. Please install version 3.x .")
         ret_cert = g_openssl111_x509_template.format(
-                    DBG_EXT = dbg_seq,
-                    SHA_OID = g_sha_oids[g_sha_to_use],
-                    SWRV = swrev,
-                    EXT_ENC_SEQ = ext_enc_seq,
-                    KD_EXT = ext_kd_seq,
-                    BOOT_CORE_ID = bootCore_id,
-                    CERT_TYPE = certType,
-                    BOOT_CORE_OPTS = bootCoreOptions,
-                    BOOT_ADDR = '{:08X}'.format(int(args.loadaddr, 16)),
-                    IMAGE_LENGTH = os.path.getsize(image_bin_name),
-                    SHA_VAL = get_sha_val(image_bin_name, g_sha_to_use),
-                    )
+            DBG_EXT=dbg_seq,
+            SHA_OID=g_sha_oids[g_sha_to_use],
+            SWRV=swrev,
+            EXT_ENC_SEQ=ext_enc_seq,
+            KD_EXT=ext_kd_seq,
+            ENC_KEYLOCK=enc_keylock_ext,
+            BOOT_CORE_ID=bootCore_id,
+            CERT_TYPE=certType,
+            BOOT_CORE_OPTS=bootCoreOptions,
+            BOOT_ADDR='{:08X}'.format(int(args.loadaddr, 16)),
+            IMAGE_LENGTH=os.path.getsize(image_bin_name),
+            SHA_VAL=get_sha_val(image_bin_name, g_sha_to_use),
+        )
 
     elif "3." in openssl_version:
         print(f"INFO: OpenSSL version {openssl_version.split()[1]} found.")
         ret_cert = g_openssl3_x509_template.format(
-                    DBG_EXT = dbg_seq,
-                    SHA_OID = g_sha_oids[g_sha_to_use],
-                    SWRV = swrev,
-                    EXT_ENC_SEQ = ext_enc_seq,
-                    KD_EXT = ext_kd_seq,
-                    BOOT_CORE_ID = bootCore_id,
-                    CERT_TYPE = certType,
-                    BOOT_CORE_OPTS = bootCoreOptions,
-                    BOOT_ADDR = '{:08X}'.format(int(args.loadaddr, 16)),
-                    IMAGE_LENGTH = os.path.getsize(image_bin_name),
-                    SHA_VAL = get_sha_val(image_bin_name, g_sha_to_use),
-                    )
+            DBG_EXT=dbg_seq,
+            SHA_OID=g_sha_oids[g_sha_to_use],
+            SWRV=swrev,
+            EXT_ENC_SEQ=ext_enc_seq,
+            KD_EXT=ext_kd_seq,
+            ENC_KEYLOCK=enc_keylock_ext,
+            BOOT_CORE_ID=bootCore_id,
+            CERT_TYPE=certType,
+            BOOT_CORE_OPTS=bootCoreOptions,
+            BOOT_ADDR='{:08X}'.format(int(args.loadaddr, 16)),
+            IMAGE_LENGTH=os.path.getsize(image_bin_name),
+            SHA_VAL=get_sha_val(image_bin_name, g_sha_to_use),
+        )
     else:
-        print(f"ERROR: OpenSSL version {openssl_version.split()[1]} found is not compatible. Please install version 1.1.1 or 3.x to continue.")
+        print(
+            f"ERROR: OpenSSL version {openssl_version.split()[1]} found is not compatible. Please install version 1.1.1 or 3.x to continue.")
         sys.exit()
 
     if args.sbl_enc:
@@ -291,18 +323,23 @@ def get_cert(args):
     elif args.tifs_enc:
         ret_cert += tifs_enc_seq
 
+    if(args.core == 'R5'):
+        ret_cert += ext_enc_keylock
+
     if(args.kd_salt and args.sbl_enc):
         ret_cert += kd_seq
 
     if(dbg_seq != ''):
         ret_cert += g_dbg_seq.format(
-                DBG_DEVICE='00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-                DBG_TYPE=g_dbg_types[args.debug],
-                 )
+            DBG_DEVICE='00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+            DBG_TYPE=g_dbg_types[args.debug],
+        )
     return dedent(ret_cert)
 
+
 def get_enc_filename(fname):
-	return fname+"-enc"
+    return fname+"-enc"
+
 
 def get_encrypted_file_iv_rs(bin_file_name, enc_key):
     if((enc_key is None) or (not os.path.exists(enc_key))):
@@ -331,7 +368,7 @@ def get_encrypted_file_iv_rs(bin_file_name, enc_key):
         v_TEST_IMAGE_ENC_RS = binascii.hexlify(enc_rs).decode('ascii')
 
         # Pad zeros to a temporary binary to make the size multiple of 16
-        zeros_pad = bytearray( 16 - (os.path.getsize(bin_file_name) % 16))
+        zeros_pad = bytearray(16 - (os.path.getsize(bin_file_name) % 16))
         tempfile_name = "tmpfile" + str(randint(1111, 9999))
         encbin_name = get_enc_filename(bin_file_name)
 
@@ -344,12 +381,14 @@ def get_encrypted_file_iv_rs(bin_file_name, enc_key):
             f.write(enc_rs)
 
         # Finally generate the encrypted image
-        subprocess.check_output('openssl aes-256-cbc -e -K {} -iv {} -in {} -out {} -nopad'.format(enckey, enc_iv, tempfile_name, encbin_name), shell=True)
+        subprocess.check_output('openssl aes-256-cbc -e -K {} -iv {} -in {} -out {} -nopad'.format(
+            enckey, enc_iv, tempfile_name, encbin_name), shell=True)
 
         # Delete the tempfile
         os.remove(tempfile_name)
 
         return encbin_name, v_TEST_IMAGE_ENC_IV, v_TEST_IMAGE_ENC_RS
+
 
 def get_key_derivation_salt(kd_salt_file_name):
     if(not os.path.exists(kd_salt_file_name)):
@@ -364,20 +403,33 @@ def get_key_derivation_salt(kd_salt_file_name):
 
     return kd_salt
 
-# MAIN
-my_parser = argparse.ArgumentParser(description="Creates a ROM-boot-able images for sbl and hsmRt")
 
-my_parser.add_argument('--image-bin',   type=str, required=True, help='Path to the SBL/hsmRT binary')
-my_parser.add_argument('--core',        type=str, help='R5/HSM are the options to build for specific core')
-my_parser.add_argument('--sbl-enc',     action='store_true', required=False, help='Encrypt SBL or not')
-my_parser.add_argument('--tifs-enc',    action='store_true', required=False, help='Encrypt TIFS-MCU or not')
-my_parser.add_argument('--enc-key',     type=str, required=False, help='Path to the SBL Encryption Key')
-my_parser.add_argument('--swrv',        type=str, help='Software revision number')
-my_parser.add_argument('--loadaddr',    type=str, required=True, help='Load address at which SBL/hsmRT needs to be loaded')
-my_parser.add_argument('--sign-key',    type=str, required=True, help='Path to the signing key to be used while creating the certificate')
-my_parser.add_argument('--kd-salt' ,    type=str, required=False, help='Path to the salt required to calculate derived key from manufacturers encryption key')
-my_parser.add_argument('--out-image',   type=str, required=True, help='Output file of SBL/hsmRT images')
-my_parser.add_argument('--debug',       type=str, help='Debug options for the image')
+# MAIN
+my_parser = argparse.ArgumentParser(
+    description="Creates a ROM-boot-able images for sbl and hsmRt")
+
+my_parser.add_argument('--image-bin',   type=str,
+                       required=True, help='Path to the SBL/hsmRT binary')
+my_parser.add_argument('--core',        type=str,
+                       help='R5/HSM are the options to build for specific core')
+my_parser.add_argument('--sbl-enc',     action='store_true',
+                       required=False, help='Encrypt SBL or not')
+my_parser.add_argument('--tifs-enc',    action='store_true',
+                       required=False, help='Encrypt TIFS-MCU or not')
+my_parser.add_argument('--enc-key',     type=str,
+                       required=False, help='Path to the SBL Encryption Key')
+my_parser.add_argument('--swrv',        type=str,
+                       help='Software revision number')
+my_parser.add_argument('--loadaddr',    type=str, required=True,
+                       help='Load address at which SBL/hsmRT needs to be loaded')
+my_parser.add_argument('--sign-key',    type=str, required=True,
+                       help='Path to the signing key to be used while creating the certificate')
+my_parser.add_argument('--kd-salt',    type=str, required=False,
+                       help='Path to the salt required to calculate derived key from manufacturers encryption key')
+my_parser.add_argument('--out-image',   type=str,
+                       required=True, help='Output file of SBL/hsmRT images')
+my_parser.add_argument('--debug',       type=str,
+                       help='Debug options for the image')
 
 args = my_parser.parse_args()
 
@@ -386,14 +438,15 @@ cert_str = get_cert(args)
 cert_file_name = "temp_cert"+str(randint(111, 999))
 
 with open(cert_file_name, "w+") as f:
-	f.write(cert_str)
+    f.write(cert_str)
 
 cert_name = "cert"+str(randint(111, 999))
 
 out_name = args.out_image
 
 # Generate the certificate
-subprocess.check_output('openssl req -new -x509 -key {} -nodes -outform DER -out {} -config {} -{}'.format(args.sign_key, cert_name, cert_file_name, g_sha_to_use), shell=True)
+subprocess.check_output('openssl req -new -x509 -key {} -nodes -outform DER -out {} -config {} -{}'.format(
+    args.sign_key, cert_name, cert_file_name, g_sha_to_use), shell=True)
 
 # Concatenate the certificate and  input binary
 final_fh = open(args.out_image, 'wb+')
