d32f57d16535180d39cfb6057353a8496cf44422
seema <seema.khatib@ti.com>
2025-08-04 15:03:08 +0530

    f29hx: misra: fix KW warning
    -MISRA.FUNC.NOPROT.DEF.2012
    
    -Function has a definition but no prototype
    
    Fixes: TIFSMCU-4046
    
    Signed-off-by: seema <seema.khatib@ti.com>

--- drivers/secure_ipc_notify/sipc_notify_src.h
+++ drivers/secure_ipc_notify/sipc_notify_src.h
@@ -117,6 +117,9 @@ extern SIPC_SwQueue* gSIPC_QueHsmToSecureHost[MAX_SEC_CORES_WITH_HSM - 1];
  */
 int32_t SIPC_Register_Isr(HwiP_Params *pHwiParams, SIPC_InterruptConfig *pInterruptConfig, SIPC_Params *params, HwiP_FxnCallback callback);
 
+/* Secure IPC Isr */
+void SIPC_isr(void *args);
+
 #ifdef __cplusplus
 }
 #endif
